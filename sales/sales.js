d75c1f647aa38b45084b6073a8df3e22fd966b6e
Anup Singh <anup.matrix@gmail.com>
2017-05-26 19:26:25 +0530

    changes header

--- sales/sales.js
+++ sales/sales.js
@@ -311,13 +311,17 @@ angular.module('salesApp.sales', ['ngRoute' , 'smart-table', 'ui.bootstrap'])
     
     $scope.printPage = function(){
         var printContents = document.getElementById("invoice-modal-full-123").innerHTML;
-        var popupWin = window.open("print.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");        
-        popupWin.window.onload = function() {
-            popupWin.document.getElementById("masterContent").innerHTML = printContents;
-            popupWin.window.print();
-            popupWin.window.close();  
-            //$scope.reloadSalesPage();
-        }        
+        var popupWin = window.open("print.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");    
+        if(popupWin){
+            popupWin.window.onload = function() {
+                popupWin.document.getElementById("masterContent").innerHTML = printContents;
+                popupWin.window.print();
+                popupWin.window.close();  
+                //$scope.reloadSalesPage();
+            };
+        }else{
+            alert("Plese disable your pop-up blocker.. for this domain");
+        }
     }
     
     $scope.reloadSalesPage = function(){
